f767aa85c44e5fdcc758057370a19dc78919f95f
pope22192 <pope22192@yahoo.com>
2017-02-05 15:20:10 -0500

    created fleet of aliens
    Created fleet of aliens, they still don’t move, but they are nicely
    spaced out and ready for the next section in the book, which includes
    code that makes the aliens move.

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,21 +14,20 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion by Pope")
 
-	# Make a ship.
+	# Make a ship, a group to store bullets in, and a group of aliens.
 	ship = Ship(ai_settings, screen)
-
-	# Make a group to store bullets in.
 	bullets = Group()
+	aliens = Group()
 
-	# Make an alien.
-	alien = Alien(ai_settings, screen)
+	# Create the fleet of Aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 
 	# Start the main loop for the game.
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
 		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, alien, bullets)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
 
