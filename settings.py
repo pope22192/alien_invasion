a565c7143b836083ce9822bcb16d6af90281c588
pope22192 <pope22192@yahoo.com>
2017-02-05 16:43:38 -0500

    updated code so fleet of aliens now move directions
    Updated code so now fleet of aliens move from right to left, and then
    drop down.

--- settings.py
+++ settings.py
@@ -7,6 +7,11 @@ class Settings():
 		self.screen_width = 1200
 		self.screen_height = 600
 		self.bg_color = (230, 230, 230)
+		# Alien settings.
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
 
 		# Bullet settings
 		self.bullet_speed_factor = 1
