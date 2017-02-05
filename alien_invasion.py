a94a946a0138f68b5c0b721db3c4f605f9f757ae
pope22192 <pope22192@yahoo.com>
2017-02-04 20:22:39 -0500

    finishing touches from chapter 12 of book
    There were some additional changes that needed to happen while
    finishing Chapter 12 of Python Crash Course by Eric Matthes.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -10,17 +11,21 @@ def run_game():
 	ai_settings = Settings()
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion")
+	pygame.display.set_caption("Alien Invasion by Pope")
 
 	# Make a ship.
 	ship = Ship(ai_settings, screen)
 
+	# Make a group to store bullets in.
+	bullets = Group()
+
 	# Start the main loop for the game.
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
-
+		gf.update_bullets(bullets)
+		gf.update_screen(ai_settings, screen, ship, bullets)
+		
 run_game()
 
 
--- settings.py
+++ settings.py
@@ -8,5 +8,12 @@ class Settings():
 		self.screen_height = 600
 		self.bg_color = (230, 230, 230)
 
+		# Bullet settings
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60,60,60
+		self.bullets_allowed = 3
+
 		# Ship settings
 		self.ship_speed_factor = 1.5
